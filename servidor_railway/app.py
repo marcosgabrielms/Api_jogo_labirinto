61e555f733fc9fbb2db9030fd604e55c35ee1373
marcosgabrielms <marcosgabriel166@gmail.com>
2025-09-30 14:25:58 -0300

    :wrench: chore: Estrutura a aplicação principal do servidor Flask

--- servidor_railway/app.py
+++ servidor_railway/app.py
@@ -1,12 +1,22 @@
+import sys
+import os
 from flask import Flask
 from flask_cors import CORS
 
+# --- INÍCIO DA CORREÇÃO ---
+# Adiciona a pasta raiz (API_JOGO_LABIRINTO) aos locais onde o Python procura módulos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# --- FIM DA CORREÇÃO ---
+
 from dashboard.routes import dashboard_bp
 
-app = Flask(__name__)
-CORS(app) # Habilita CORS para todas as rotas
+# --- CORREÇÃO AQUI ---
+# Informa ao Flask que a pasta 'static' está um nível acima
+app = Flask(__name__, static_folder='../static')
+
+CORS(app) 
 
 app.register_blueprint(dashboard_bp)
 
-if __name__ - '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
